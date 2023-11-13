e346ac9e8476a00aa3f9538229c7ae4b2c85c977
Marilyne Naili <marilyne.lydia@gmail.com>
2023-11-13 15:28:19 +0100

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TacheComponent } from './tache/tache.component';
+import { ClientComponent } from './client/client.component';
+import { AdminComponent } from './admin/admin.component';
+import { StatsComponent } from './stats/stats.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'tache', component: TacheComponent},
+  {path: 'client', component:ClientComponent},
+  {path: 'admin', component: AdminComponent},
+  {path: 'stats', component: StatsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
