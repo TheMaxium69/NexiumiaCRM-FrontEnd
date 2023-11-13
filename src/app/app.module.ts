0fa46e031db975c3e9c70a43bd08ccea86e83584
Marilyne Naili <marilyne.lydia@gmail.com>
2023-11-13 14:37:09 +0100

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { TacheComponent } from './tache/tache.component';
+import { ClientComponent } from './client/client.component';
+import { AdminComponent } from './admin/admin.component';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ConnexionComponent,
+    TacheComponent,
+    ClientComponent,
+    AdminComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
