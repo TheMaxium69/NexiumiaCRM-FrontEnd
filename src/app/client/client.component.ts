29d171db428eb94ca2273954f80239f8a8c698b3
Maxime Tournier <mxm.tournier@gmail.com>
2023-11-30 01:00:28 +0100

    update

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {AppComponent} from "../app.component";
 import {ClientService} from "../client.service";
 import {ClientInterface} from "../client-interface";
+import {AgencyService} from "../agency.service";
+import {AgencyInterface} from "../agency-interface";
 
 @Component({
   selector: 'app-client',
@@ -13,9 +15,16 @@ export class ClientComponent implements OnInit{
   title: string = 'Clients';
   description: string = 'je suis la description a la page';
   clientAll: ClientInterface[]|any;
+  agenceAll: AgencyInterface[]|any;
+
+  termeRecherche: string = '';
+  clientRechercher: ClientInterface[]|any;
+  selectedAgency: AgencyInterface[] = [];
+  isSelectedAngency: number = 0;
 
   constructor(private app: AppComponent,
-              private clientService: ClientService) { }
+              private clientService: ClientService,
+              private agenceService: AgencyService) { }
 
   ngOnInit(){
 
@@ -23,11 +32,44 @@ export class ClientComponent implements OnInit{
     this.app.verifToken();
 
     this.clientService.getClients(this.app.setURL(),this.app.createCorsToken()).subscribe(clients => {
-
       this.clientAll = clients;
-      console.log(this.clientAll);
-
+      this.clientRechercher = this.clientAll;
+    })
+    this.agenceService.getAgencys(this.app.setURL(),this.app.createCorsToken()).subscribe(agencys => {
+      this.agenceAll = agencys;
     })
   }
+
+// BAR DE RECHERCHE
+  searchClient() {
+
+    this.clientRechercher = this.clientAll.filter((client: ClientInterface) =>
+      client.lastName.toLowerCase().includes(this.termeRecherche.toLowerCase()) ||
+      client.firstName.toLowerCase().includes(this.termeRecherche.toLowerCase()) ||
+      client.phone.toLowerCase().includes(this.termeRecherche.toLowerCase()) ||
+      client.email.toLowerCase().includes(this.termeRecherche.toLowerCase())
+    );
+
+  }
+
+
+// FILTRE AGENCE
+
+  toggleSelection(OneAgencySelected: AgencyInterface) {
+    const index = this.selectedAgency.findIndex(selectedAgency => selectedAgency.id === OneAgencySelected.id);
+
+    if (index !== -1) {
+      this.selectedAgency.splice(index, 1);
+    } else {
+      this.selectedAgency.push(OneAgencySelected);
+    }
+
+    this.isSelectedAngency = this.selectedAgency.length;
+  }
+
+  isSelected(OneAgencySelected: AgencyInterface): boolean {
+    return this.selectedAgency.some(selectedAgency => selectedAgency.id === OneAgencySelected.id);
+  }
+
 
 }
